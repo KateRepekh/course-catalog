d2beee6ce29d8b37f1dc6a1f5dfbadb34499ca9d
KateRepekh <krepekh@gmail.com>
2021-05-05 06:02:42 +0300

    Make trailing slash optional

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,11 @@ from rest_framework.routers import DefaultRouter
 
 from courses.views import CourseViewSet
 
+class OptionalSlashRouter(DefaultRouter):      
+    def __init__(self, *args, **kwargs):         
+        super(DefaultRouter, self).__init__(*args, **kwargs)         
+        self.trailing_slash = '/?' 
 
-router = DefaultRouter()
+router = OptionalSlashRouter()
 router.register("courses", CourseViewSet)
 urlpatterns = router.urls
